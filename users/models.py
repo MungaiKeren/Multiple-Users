8fd25fdde9974e99f6c950f1955671ca7b253cda
MungaiKeren <wambukeren@gmail.com>
2020-04-03 01:33:42 +0300

    Create a custom user model inheriting from the Abstract BaseUser

--- users/models.py
+++ users/models.py
@@ -1,57 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractBaseUser
+)
 
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    # full_name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True) #can login
+    staff = models.BooleanField(default=False) # not super user
+    admin = models.BooleanField(default=False) #superuser
 
-# Create your models here.
-class UserManager(BaseUserManager):
-    def _create_user(self, email, password, is_staff, is_superuser):
-        if not email:
-            raise ValueError("Users must have an email address")
+    USERNAME_FIELD = "email" #username
+    # username and password field are required by default
+    REQUIRED_FIELDS = [] #["full name"] Would appear in python manage.py createsuperuser
 
-        now = timezone.now()
-        email = self.normalize_email(email)
-        user = self.model(
-            email = email,
-            is_staff = is_staff,
-            is_active = True,
-            is_superuser = is_superuser,
-            last_login = now,
-            date_joined = now,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_user(self, email=None, password = None):
-        return self._create_user(email, password, False, False)
-    
-    def create_superuser(self, email, password):
-        user = self._create_user(email, password, True, True)
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=254, unique=True)
-    name = models.CharField(max_length=254, null=True, blank=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    last_login = models.DateTimeField(null=True, blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    USERNAME_FIELD = email
-    EMAIL_FIELD = email
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
-
-    def get_absolute_url(self):
-        return "/users/%i/" % (self.pk)
-    
-    def get_email(self):
+    def __str__(self):
         return self.email
 
-
+    def get_full_name(self):
+        return self.email
+    
+    def get_short_name(self):
+        return self.email
+    
+    @property
+    def is_staff(self):
+        return self.staff
+    
+    @property
+    def is_admin(self):
+        return self.admin
+    
+    @property
+    def is_active(self):
+        return self.active
