16c2d87590b886b6aecdcf7c355db43496e64095
MungaiKeren <wambukeren@gmail.com>
2020-04-14 13:01:05 +0300

    create registration and login forms for the users

--- users/forms.py
+++ users/forms.py
@@ -44,3 +44,35 @@ class UserAdminChangeForm(forms.ModelForm):
         # This is done here rather than on the field, 
         # because the field does not have the initial value 
         return self.initial["password"]
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label="Email")
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class RegisterForm(forms.ModelForm):
+    """A form for creating users"""
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("email","full_name")
+    
+    def clean_password2(self):
+        # check that the two password fields match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+    
+    def save(self, commit=True):
+        # save the provided password in hashed format
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.active = False # send confirmation Email
+
+        if commit:
+            user.save()
+        return user
