a0cea5be916e6665d35f5b83cf0c34d152ee0f10
MungaiKeren <wambukeren@gmail.com>
2020-04-19 11:23:41 +0300

    start a new app that will use ajax with django

--- users/views.py
+++ users/views.py
@@ -2,14 +2,41 @@ from django.shortcuts import render,redirect
 from .models import *
 from .forms import *
 from django.views.generic import CreateView, FormView
+from django.contrib.auth import authenticate, login, get_user_model
+from django.utils.http import is_safe_url
 
-User = get_user_model()
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    if form.is_valid():
-        form.save()
+class LoginView(FormView):
+    form_class = LoginForm
+    success_url = "/"
+    template_name = "accounts/login.html"
 
-    return render(request, "accounts/register.html", context)
+    def form_valid(self, form):
+        request = self.request
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=email, password=password)
+        print(user)
+
+        if user is not None:
+            login(request, user)
+            try:
+                del request.session['guest_email_id']
+            except:
+                pass
+
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
+            else:
+                return redirect("/")
+        return super(LoginView, self).form_invalid(form)
+
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "accounts/register.html"
+    success_url = '/login/'
